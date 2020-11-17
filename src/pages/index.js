961b38564e4e188d9ad796efba4287191204afb6
Brian Long <brianlong912@gmail.com>
2020-11-16 23:51:15 -0700

    clicking on image shows larger overlay

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,12 @@ import "../styles/index.css"
 
 export default function IndexPage({ data }) {
   const [numPicsWide, setNumPicsWide] = useState(3)
+  const [modalImageId, setModalImageId] = useState(
+    data.allImageSharp.edges[0].node.id
+  )
   let picWidth = 100 / numPicsWide
 
-  /*Functions to incrase and decrease the size of the pictures on the screen */
+  /* Functions to incrase and decrease the size of the pictures on the screen */
   function increasePics() {
     if (numPicsWide > 1) {
       setNumPicsWide(numPicsWide - 2)
@@ -22,21 +25,44 @@ export default function IndexPage({ data }) {
     }
   }
 
-  /*Creates an array of all of the photos from the graphql query */
-  const pics = data.allFile.edges.map(edge => {
-    const pic = edge.node.childImageSharp.fluid
+  /* Find the correct image for the modal overlay of a selected iamge */
+  const modalImage = data.allImageSharp.edges.find(e => {
+    return e.node.id === modalImageId
+  })
+
+  /* function to toggle modal and set image for modal */
+  function showImage(id) {
+    document.getElementById("modal").style.display = "block"
+    setModalImageId(id)
+  }
+
+  /* add event listener to modal window for user clicking off of image */
+  var modalWindow = document.getElementById("modal")
+  if (modalWindow) {
+    modalWindow.addEventListener("click", function (e) {
+      const modalPic = document.getElementById("modalPic")
+      if (!modalPic.contains(e.target)) {
+        modalWindow.style.display = "none"
+      }
+    })
+  }
+
+  /*Creates an array of elements of all of the photos from the graphql query */
+  const pics = data.allImageSharp.edges.map(edge => {
+    const pic = edge.node.square
     return (
-      <div
-        key={pic.src}
+      <button
+        key={edge.id}
         className="pic-wrapper"
         style={{ width: picWidth + "%", overflow: "hidden" }}
+        onClick={() => showImage(edge.node.id)}
       >
         <Img
           fluid={pic}
           className={numPicsWide === 1 ? "pic-full" : "pic"}
           imgStyle={{ transition: "transform .5s" }}
         />
-      </div>
+      </button>
     )
   })
 
@@ -48,7 +74,7 @@ export default function IndexPage({ data }) {
           position: "fixed",
           left: "30px",
           bottom: "30px",
-          zIndex: "99",
+          zIndex: "98",
           display: "flex",
           flexDirection: "column",
           width: "32px",
@@ -62,25 +88,48 @@ export default function IndexPage({ data }) {
         </button>
       </div>
       <div style={{ display: "flex", flexWrap: "wrap" }}>{pics}</div>
+      <div
+        id="modal"
+        style={{
+          display: "none",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(51,51,51,.75)",
+          // opacity: "75%",
+          zIndex: "99",
+        }}
+      >
+        <div
+          id="modalPic"
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Img fixed={modalImage.node.originalAspect} />
+        </div>
+      </div>
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-    allFile(
-      filter: {
-        extension: { regex: "/(png|jpg)/" }
-        sourceInstanceName: { eq: "images" }
-      }
-    ) {
+    allImageSharp {
       edges {
         node {
-          childImageSharp {
-            fluid(maxWidth: 500, maxHeight: 500) {
-              ...GatsbyImageSharpFluid
-            }
+          square: fluid(maxWidth: 1000, maxHeight: 1000) {
+            ...GatsbyImageSharpFluid
           }
+          originalAspect: fixed(height: 700) {
+            ...GatsbyImageSharpFixed
+          }
+          id
         }
       }
     }
